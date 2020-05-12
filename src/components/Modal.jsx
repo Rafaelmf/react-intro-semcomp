bd9fbe71d81e8930e5bb9b193f94fccc58231efa
Rafael <rafael.martins@xmobots.com.br>
2020-05-12 20:07:57 -0300

    Ajustes na modal

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -1,17 +1,28 @@
-import React from 'react';
+import React from "react";
 
 function Modal(props) {
   const { isModalOpen, onCloseModal } = props;
   return (
     <div
       id="myModal"
-      className={isModalOpen ? 'modal' : 'modal display-none-modal'}
+      className={isModalOpen ? "modal" : "modal display-none-modal"}
     >
       <div className="modal-content">
-        <button type="button" onClick={() => onCloseModal()} className="close">
-          X
-        </button>
-        <p>Some text in the Modal..</p>
+        <div className="modal-header">
+          <span>Adicionar nova nota</span>
+          <span type="button" onClick={() => onCloseModal()} className="close">
+            x
+          </span>
+        </div>
+
+        <form action="">
+          <label htmlFor="title">Título</label>
+          <input type="text" id="title" name="firstname" />
+
+          <label htmlFor="content">Conteúdo</label>
+          <textarea rows="5" id="content" name="lastname" />
+          <input type="submit" value="Adicionar" />
+        </form>
       </div>
     </div>
   );
--- src/components/NotesTable.jsx
+++ src/components/NotesTable.jsx
@@ -1,8 +1,8 @@
-import React from 'react';
+import React from "react";
 
-// import { Container } from './styles';
-
-function NotesTable() {
+function NotesTable(props) {
+  const { notesList } = props;
+  console.log(notesList);
   return (
     <table id="created-notes">
       <thead>
@@ -14,67 +14,18 @@ function NotesTable() {
         </tr>
       </thead>
       <tbody>
-        <tr>
-          <td>Alfreds Futterkiste</td>
-          <td>
-            Maria Andaria Andaria Andaria Andaria Andaria Andaria Andaria
-            Andaria Andaria Andaria Andaria Andaria Andaria Andaria Andaria
-            Andaria Andaria Andaria Andaria Andaria Andaria Andaria Andaria
-            Andaria Andaria Andaria Andaria Andaria Andaria Andaria Andaria
-            Andaria Andaria Andaria Andaria Andaria Andaria Andaria Andaria
-            Andaria Andaria Anders
-          </td>
-          <td>{new Date(Date.now()).toLocaleString().split(',')[0]}</td>
-          <td>
-            <button type="button">Remover</button>
-          </td>
-        </tr>
-        <tr>
-          <td>Berglunds snabbköp</td>
-          <td>Christina Berglund</td>
-          <td>{new Date(Date.now()).toLocaleString().split(',')[0]}</td>
-          <td>
-            <button type="button">Remover</button>
-          </td>
-        </tr>
-        <tr>
-          <td>Centro comercial Moctezuma</td>
-          <td>Francisco Chang</td>
-          <td>{new Date(Date.now()).toLocaleString().split(',')[0]}</td>
-          <td>
-            <button type="button">Remover</button>
-          </td>
-        </tr>
-        <tr>
-          <td></td>
-          <td>Ernst Handel</td>
-          <td>Roland Mendel</td>
-          <td>Austria</td>
-        </tr>
-        <tr>
-          <td></td>
-          <td>Island Trading</td>
-          <td>Helen Bennett</td>
-          <td>UK</td>
-        </tr>
-        <tr>
-          <td></td>
-          <td>Königlich Essen</td>
-          <td>Philip Cramer</td>
-          <td>Germany</td>
-        </tr>
-        <tr>
-          <td></td>
-          <td>Laughing Bacchus Winecellars</td>
-          <td>Yoshi Tannamuri</td>
-          <td>Canada</td>
-        </tr>
-        <tr>
-          <td></td>
-          <td>Magazzini Alimentari Riuniti</td>
-          <td>Giovanni Rovelli</td>
-          <td>Italy</td>
-        </tr>
+        {notesList
+          ? notesList.map((note) => (
+              <tr>
+                <td></td>
+                <td></td>
+                <td>{new Date(Date.now()).toLocaleString().split(",")[0]}</td>
+                <td>
+                  <button type="button">Remover</button>
+                </td>
+              </tr>
+            ))
+          : null}
       </tbody>
     </table>
   );
